09012a9d628a3046fe7abdb416eabc468ed0426b
Youness <mansaryouness@gmail.com>
2020-10-18 12:28:41 +0200

    add lambda = 2 in plot

--- eval_data/plot_loss.py
+++ eval_data/plot_loss.py
@@ -4,12 +4,14 @@ from matplotlib import pyplot as plt
 
 df0 = pd.read_csv("run-Lambda=0.0-tag-valid_loss_y.csv")
 df10 = pd.read_csv("run-Lambda=10.0-tag-valid_loss_y.csv")
+df2 = pd.read_csv("run-Lambda=2.0-tag-valid_loss_y.csv")
 
 
 plt.figure()
 
 plt.plot(df0["Step"], df0["Value"], label="Baseline")
 plt.plot(df10["Step"], df10["Value"], label=r"$\lambda$ = 10")
+plt.plot(df2["Step"], df2["Value"], label=r"$\lambda$ = 2")
 
 plt.xlabel("Step")
 plt.ylabel("Loss (Lower is better)")
