2a2b52b44feaa99f9cfad7c50b89a17b5c8f4cf9
Youness <mansaryouness@gmail.com>
2020-10-18 14:26:34 +0200

    run.sh update

--- audio/run.sh
+++ audio/run.sh
@@ -1,5 +1,14 @@
-#python prepare_data.py --metadata_path "/media/ml/data_ml/fma_metadata/"
-#python audio_processing.py --mp3_path "/media/ml/data_ml/fma_small/"
+# Data download:
+
+#wget -c https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
+#wget -c https://os.unil.cloud.switch.ch/fma/fma_small.zip
+
+# Data preparation
+
+python prepare_data.py --metadata_path "/media/ml/data_ml/fma_metadata/"
+python audio_processing.py --mp3_path "/media/ml/data_ml/fma_small/"
+
+# Training
 
 python lstm_genre_classification.py --metadata_path "/media/ml/data_ml/fma_metadata/" \
   --mp3_path "/media/ml/data_ml/fma_small/" \
@@ -33,6 +42,7 @@ python lstm_genre_classification.py --metadata_path "/media/ml/data_ml/fma_metad
   --mp3_path "/media/ml/data_ml/fma_small/" \
   --reconstruction_weight 0.0
 
+# Evaluation
 
 python eval_lstm_genre_classification.py --metadata_path "/media/ml/data_ml/fma_metadata/" \
   --mp3_path "/media/ml/data_ml/fma_small/"
